4a07cff303893ec921e0ed0110d447f6facc9c04
garydblack <gary.black@pnnl.gov>
2022-03-25 16:53:33 -0700

    Cleaned up for demo and show how to take the message response Ybus to a full one with complex values.

--- static-ybus/test_sybus.py
+++ static-ybus/test_sybus.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
-import sys
-import time
-
 # gridappsd-python module
 from gridappsd import GridAPPSD
+
+
+def fullComplex(lowerUncomplex):
+  YbusComplex = {}
+
+  for noderow in lowerUncomplex:
+    for nodecol,value in lowerUncomplex[noderow].items():
+      if noderow not in YbusComplex:
+        YbusComplex[noderow] = {}
+      if nodecol not in YbusComplex:
+        YbusComplex[nodecol] = {}
+      YbusComplex[noderow][nodecol] = YbusComplex[nodecol][noderow] = complex(value[0], value[1])
+
+  return YbusComplex
+
 
 def _main():
   feeder_mrid = '_5B816B93-7A5F-B64C-8460-47C17D6E4B0F' # 13assets
@@ -17,8 +29,13 @@ def _main():
     "requestType": "GET_SNAPSHOT_YBUS",
     "feeder_id": feeder_mrid
   }
+  print('Requesting static Ybus for feeder_id: ' + feeder_mrid + '\n', flush=True)
   message = gapps.get_response(topic, request, timeout=90)
-  print('Got Ybus Snapshot response: ' + str(message) + '\n', flush=True)
+  print('Got static Ybus snapshot response: ' + str(message) + '\n', flush=True)
+
+  Ybus = fullComplex(message['ybus'])
+  print('Ybus converted from lower diagonal to full with complex values:', flush=True)
+  print(Ybus, flush=True)
 
   gapps.disconnect()
 
