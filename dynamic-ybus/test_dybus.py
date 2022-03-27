b864a113b6136bd9c8df0685bca0af936052ece6
garydblack <gary.black@pnnl.gov>
2022-03-27 12:05:36 -0700

    Updated to the refactored messaging that allows apps like this test app to get the guaranteed up-to-date Ybus with initialization.

--- dynamic-ybus/test_dybus.py
+++ dynamic-ybus/test_dybus.py
@@ -5,29 +5,80 @@ import time
 
 # gridappsd-python module
 from gridappsd import GridAPPSD
-from gridappsd.topics import simulation_output_topic, service_output_topic
-
-YbusInitializedFlag = False
+from gridappsd.topics import service_output_topic
 
 
-def ybusFullCallback(header, message):
-  if not YbusInitializedFlag:
-    return
+class DYbusTester:
+  def __init__(self, gapps, simID):
+    self.Ybus = {}
+    self.YbusPreInit = {}
+    self.timestampPreInit = 0
+    self.ybusInitFlag = False
 
-  #print('Received Ybus Full message: ' + str(message) + '\n', flush=True)
-  print('Received Ybus Full message\n', flush=True)
+    # subscribe to Dynamic Ybus changes
+    gapps.subscribe(service_output_topic('gridappsd-dynamic-ybus', simID), self)
+
+    # request/response for snapshot Ybus
+    topic = 'goss.gridappsd.request.data.dynamic-ybus.' + simID
+    request = {
+      "requestType": "GET_SNAPSHOT_YBUS"
+    }
+    print('Requesting dynamic Ybus snapshot for sim_id: ' + simID + '\n', flush=True)
+    message = gapps.get_response(topic, request, timeout=90)
+    print('Got dynamic Ybus snapshot response: ' + str(message) + '\n', flush=True)
+
+    snapshotTimestamp = message['timestamp']
+    if self.timestampPreInit > message['timestamp']:
+      print('Dynamic Ybus initialized from an update message with timestamp: ' + str(self.timestampPreInit) + ', snapshot timestamp: ' + str(message['timestamp']) + '\n', flush=True)
+      self.Ybus = self.fullComplexInit(self.YbusPreInit)
+      self.YbusPreInit = {} # free up memory no longer needed
+    else:
+      print('Dynamic Ybus initialized from snapshot response\n', flush=True)
+      self.Ybus = self.fullComplexInit(message['ybus'])
+
+    self.ybusInitFlag = True
+
+    # do any processing after Ybus is initialized here
 
 
-def ybusChangesCallback(header, message):
-  if not YbusInitializedFlag:
-    return
+  def on_message(self, header, message):
+    if not self.ybusInitFlag:
+      # save the most recent ybus message we get before request/response
+      # initialization to compare timestamps with what comes back from that
+      self.YbusPreInit = message['ybus']
+      self.timestampPreInit = message['timestamp']
+      print('Received pre-init Ybus update message with timestamp: ' + str(self.timestampPreInit) + '\n', flush=True)
 
-  print('Received Ybus Changes message: ' + str(message) + '\n', flush=True)
+    else:
+      print("Received Ybus update message with timestamp: ' + str(message['timestamp']) + ', Ybus changes: ' + str(message['ybus_changes']) + \n", flush=True)
+      self.fullComplexUpdate(message['ybus_changes'])
+
+      # do any processing after Ybus is updated here
+
+
+  def fullComplexInit(self, lowerUncomplex):
+    YbusInit = {}
+
+    for noderow in lowerUncomplex:
+      for nodecol,value in lowerUncomplex[noderow].items():
+        if noderow not in YbusInit:
+          YbusInit[noderow] = {}
+        if nodecol not in YbusInit:
+          YbusInit[nodecol] = {}
+        YbusInit[noderow][nodecol] = YbusInit[nodecol][noderow] = complex(value[0], value[1])
+    #print(YbusInit, flush=True)
+
+    return YbusInit
+
+
+  def fullComplexUpdate(self, lowerUncomplex):
+    for noderow in lowerUncomplex:
+      for nodecol,value in lowerUncomplex[noderow].items():
+        self.Ybus[noderow][nodecol] = self.Ybus[nodecol][noderow] = complex(value[0], value[1])
+    #print(self.Ybus, flush=True)
 
 
 def _main():
-  global YbusInitializedFlag
-
   if len(sys.argv) < 2:
     usestr =  '\nUsage: ' + sys.argv[0] + ' simID\n' 
     print(usestr, flush=True)
@@ -37,22 +88,7 @@ def _main():
 
   gapps = GridAPPSD()
 
-  # subscribe to all the Dynamic Ybus subscriptions
-  gapps.subscribe(service_output_topic('gridappsd-dynamic-ybus-full',
-                                                 simID), ybusFullCallback)
-  gapps.subscribe(service_output_topic('gridappsd-dynamic-ybus-changes',
-                                                 simID), ybusChangesCallback)
-
-  # request/response for snapshot Ybus
-  topic = 'goss.gridappsd.request.data.dynamic-ybus.' + simID
-  request = {
-    "requestType": "GET_SNAPSHOT_YBUS"
-  }
-  print('Requesting dynamic Ybus snapshot for sim_id: ' + simID + '\n', flush=True)
-  message = gapps.get_response(topic, request, timeout=90)
-  print('Got dynamic Ybus snapshot response: ' + str(message) + '\n', flush=True)
-
-  YbusInitializedFlag = True
+  dybus = DYbusTester(gapps, simID)
 
   while True:
     time.sleep(0.1)
