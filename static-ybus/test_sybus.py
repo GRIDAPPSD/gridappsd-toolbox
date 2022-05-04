3795cb0f0f516757eae8f26222b8d9dc81043785
garydblack <gary.black@pnnl.gov>
2022-05-04 14:41:03 -0700

    Added feeder_mrid values for running other IEEE models beyond 13assets.  These are currently commented out after testing them.

--- static-ybus/test_sybus.py
+++ static-ybus/test_sybus.py
@@ -20,6 +20,9 @@ def fullComplex(lowerUncomplex):
 
 def _main():
   feeder_mrid = '_5B816B93-7A5F-B64C-8460-47C17D6E4B0F' # 13assets
+  #feeder_mrid = '_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62' # 13
+  #feeder_mrid = '_C1C3E687-6FFD-C753-582B-632A27E28507' # 123
+  #feeder_mrid = '_AAE94E4A-2465-6F5E-37B1-3E72183A4E44' # 9500
 
   gapps = GridAPPSD()
 
